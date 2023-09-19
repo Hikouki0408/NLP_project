af6e38689bb4409e959358b892b8254535d8e2f2
Hikouki0408 <k.hirose@student.vu.nl>
2023-09-19 23:51:47 +0200

    update the model

--- train_model.py
+++ train_model.py
@@ -1,15 +1,15 @@
 import csv
 import pandas as pd
+import random
 import torch
 import torch.nn as nn
 import torch.optim as optim
-from torch.utils.data import Dataset, DataLoader
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.model_selection import train_test_split
-from nltk.corpus import stopwords
-from nltk.tokenize import word_tokenize
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
-import numpy as np
+from torch.utils.data import Dataset, DataLoader
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
 
 # Define a mapping of sentiment labels to numerical values
 sentiment_map = {'negative': 0, 'neutral': 2, 'positive': 4}
@@ -35,17 +35,14 @@ class SentimentDataset(Dataset):
 
         return torch.FloatTensor(tfidf_features), torch.LongTensor([sentiment_label])  # Return TF-IDF features and sentiment label
 
-# Define a simple LSTM-based sentiment analysis model
+# Define a simple sentiment analysis model
 class SentimentModel(nn.Module):
-    def __init__(self, input_size, hidden_size, num_layers, num_classes):
+    def __init__(self, input_size, num_classes):
         super(SentimentModel, self).__init__()
-        self.lstm = nn.LSTM(input_size, hidden_size, num_layers, batch_first=True)
-        self.fc = nn.Linear(hidden_size, num_classes)
+        self.fc = nn.Linear(input_size, num_classes)  # Define a fully connected layer
 
     def forward(self, x):
-        out, _ = self.lstm(x)
-        out = self.fc(out[:, -1, :])  # Use the output of the last time step
-        return out
+        return self.fc(x)  # Forward pass through the fully connected layer
 
 # Define a tokenizer (you can customize this further)
 def tokenize_text(text):
@@ -69,13 +66,16 @@ def map_sentiment(value):
     else:
         return 'unknown'
 
-# Define a function to read text data from a CSV file
-def read_text_from_csv(file_path):
+# Define a function to read text data from a CSV file up to a specified number of instances
+def read_text_from_csv(file_path, max_instances=None):
     try:
         text_list = []
         with open(file_path, 'r', encoding='latin-1') as csv_file:
             csv_reader = csv.reader(csv_file)
-            for row in csv_reader:
+            for idx, row in enumerate(csv_reader, start=1):
+                if max_instances is not None and idx > max_instances:
+                    break  # Stop reading after reaching the specified number of instances
+                
                 if len(row) >= 6:  # Checking if column F exists in the row
                     sentiment_label = row[0]
                     sentiment = map_sentiment(sentiment_label)
@@ -98,84 +98,60 @@ def preprocess_data(data):
     tfidf_vectorizer = TfidfVectorizer(max_features=5000, tokenizer=lambda x: x, preprocessor=lambda x: x)
 
     # Fit the vectorizer on the tokenized text data
-    tfidf_vectorizer.fit([" ".join(tokens) for _, tokens in tokenized_data])
+    tfidf_vectorizer.fit([text for sentiment, text in tokenized_data])
 
     # Map sentiment labels to numerical values
     labels = [sentiment_map[sentiment] for sentiment, _ in data]
 
     return tokenized_data, labels, tfidf_vectorizer
 
-# Define a function for model training
+# Define a function to train the model
 def train_model(train_loader, model, criterion, optimizer, num_epochs=10):
-    model.train()  # Set the model to training mode
     for epoch in range(num_epochs):
-        total_loss = 0.0
         for inputs, labels in train_loader:
             optimizer.zero_grad()  # Zero the gradients
-            outputs = model(inputs)  # Forward pass through the model
+            outputs = model(inputs.float())  # Forward pass through the model
             loss = criterion(outputs, labels.squeeze())  # Calculate the loss
             loss.backward()  # Backpropagation
             optimizer.step()  # Update model parameters
-            total_loss += loss.item()
-        average_loss = total_loss / len(train_loader)
-        print(f'Epoch [{epoch+1}/{num_epochs}], Loss: {average_loss:.4f}')
-
-# Define a function for model evaluation
-def evaluate_model(model, test_loader):
-    model.eval()  # Set the model to evaluation mode
-    y_true = []
-    y_pred = []
-    with torch.no_grad():
-        for inputs, labels in test_loader:
-            outputs = model(inputs)
-            _, predicted = torch.max(outputs.data, 1)
-            y_true.extend(labels.cpu().numpy())
-            y_pred.extend(predicted.cpu().numpy())
-
-    accuracy = accuracy_score(y_true, y_pred)
-    precision = precision_score(y_true, y_pred, average='weighted')
-    recall = recall_score(y_true, y_pred, average='weighted')
-    f1 = f1_score(y_true, y_pred, average='weighted')
-    print(f'Accuracy: {accuracy:.4f}, Precision: {precision:.4f}, Recall: {recall:.4f}, F1 Score: {f1:.4f}')
+        print(f'Epoch [{epoch+1}/{num_epochs}], Loss: {loss.item():.4f}')
 
 # Define the main function
 def main():
     file_path = 'datasets/dataset_tweet.csv'  # Update this with the actual file path
+    # Read text data from CSV up to 1,500,000 instances
+    text_list = read_text_from_csv(file_path, max_instances=15000)
 
-    text_list = read_text_from_csv(file_path)  # Read text data from CSV
+    if text_list:
+        print("Sentiment and Text from columns A and F:")
+        for idx, (sentiment, text) in enumerate(text_list, start=1):
+            print(f"{idx}: {sentiment.capitalize()}: {text}")
+    else:
+        print("Failed to read text from the CSV file.")
 
+    # Preprocess the data as before
+    tokenized_data, labels, tfidf_vectorizer = preprocess_data(text_list)
 
-    # Preprocess the data
-    labels, tfidf_vectorizer = preprocess_data(text_list)
-    print("Here 1")
     # Split the data into training and testing sets
-    X_train, X_test, y_train, y_test = train_test_split(tfidf_vectorizer.transform([" ".join(tokens) for _, tokens in text_list]), labels, test_size=0.2, random_state=42)
-    print("Here 2")
-    # Create data loaders for batch processing
-    train_dataset = torch.utils.data.TensorDataset(torch.FloatTensor(X_train.toarray()), torch.LongTensor(y_train))
+    X_train, X_test, y_train, y_test = train_test_split(tokenized_data, labels, test_size=0.2, random_state=42)
+
+    # Create data loaders
+    train_dataset = SentimentDataset(pd.DataFrame(X_train, columns=['sentiment', 'text']), tokenize_text, tfidf_vectorizer)
     train_loader = DataLoader(train_dataset, batch_size=32, shuffle=True)
-    print("Here 3")
-    test_dataset = torch.utils.data.TensorDataset(torch.FloatTensor(X_test.toarray()), torch.LongTensor(y_test))
-    test_loader = DataLoader(test_dataset, batch_size=32, shuffle=False)
-    print("Here 4")
+
     # Define model parameters
-    input_size = X_train.shape[1]
-    hidden_size = 128
-    num_layers = 2
+    input_size = len(tfidf_vectorizer.get_feature_names_out())
     num_classes = len(set(labels))
 
     # Initialize the model and optimizer
-    model = SentimentModel(input_size, hidden_size, num_layers, num_classes)
+    model = SentimentModel(input_size, num_classes)
     criterion = nn.CrossEntropyLoss()
     optimizer = optim.Adam(model.parameters(), lr=0.001)
 
-    print("Here 5")
     # Train the model
     train_model(train_loader, model, criterion, optimizer, num_epochs=10)
 
-    # Evaluate the model
-    print("Model Evaluation:")
-    evaluate_model(model, test_loader)
+    # Continue with evaluation and inference as needed
 
 if __name__ == "__main__":
     main()
