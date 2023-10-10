0eb5c166b6b1142213180dc34ba21ab6b9f19608
KOKI Hirose <76944079+Hikouki0408@users.noreply.github.com>
2023-10-10 05:10:37 +0200

    implemented hyperparameter tuning

--- detect_tweet.py
+++ detect_tweet.py
@@ -3,13 +3,14 @@ import nltk
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
 from nltk.stem import PorterStemmer
-from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.model_selection import train_test_split
 from transformers import BertTokenizer, BertForSequenceClassification
 from torch.utils.data import DataLoader, TensorDataset
 import torch
 from sklearn.metrics import classification_report
 from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import f1_score
 
 # Download NLTK stopwords if you haven't already
 nltk.download('stopwords')
@@ -74,7 +75,7 @@ def read_text_from_csv(file_path, max_instances=100):
         return None
 
 # Function to fine-tune a BERT model for sentiment analysis
-def fine_tune_bert(X_train, y_train):
+def fine_tune_bert(X_train, y_train, lr=1e-5, batch_size=16, num_epochs=3):
     # Load pre-trained BERT model and tokenizer
     model_name = "bert-base-uncased"
     tokenizer = BertTokenizer.from_pretrained(model_name)
@@ -85,18 +86,17 @@ def fine_tune_bert(X_train, y_train):
 
     # Encode labels (negative, neutral, positive)
     label_encoder = LabelEncoder()
-    y_train_encoded = label_encoder.fit_transform(y_train)  # Define label_encoder here
+    y_train_encoded = label_encoder.fit_transform(y_train)
 
     # Create DataLoader for batch processing
     train_data = TensorDataset(X_train_encoded.input_ids, X_train_encoded.attention_mask, torch.tensor(y_train_encoded))
-    train_dataloader = DataLoader(train_data, batch_size=16, shuffle=True)
+    train_dataloader = DataLoader(train_data, batch_size=batch_size, shuffle=True)
 
-    # Define optimizer and loss function (you may need to fine-tune hyperparameters)
-    optimizer = torch.optim.AdamW(model.parameters(), lr=1e-5)
+    # Define optimizer and loss function
+    optimizer = torch.optim.AdamW(model.parameters(), lr=lr)
     loss_fn = torch.nn.CrossEntropyLoss()
 
-    # Training loop (you may need to adjust the number of epochs and other hyperparameters)
-    num_epochs = 3
+    # Training loop
     for epoch in range(num_epochs):
         model.train()
         for batch in train_dataloader:
@@ -144,43 +144,83 @@ def evaluate_bert(model, X_test, y_test):
 # Main function
 def main():
     file_path = 'datasets/dataset_tweet.csv'  # Update this with the actual file path
-    max_instances = 1000  # Specify the maximum number of instances to read
+    max_instances = 16000  # Specify the maximum number of instances to read
+    #1600000
 
     text_list = read_text_from_csv(file_path, max_instances)
 
     if text_list:
         print("Sentiment and Text from columns A and F (preprocessed):")
+        """
         for idx, (sentiment, text) in enumerate(text_list, start=1):
             print(f"{sentiment.capitalize()}: {text}")
 
             # Test print statement to check preprocessing
             original_text = read_text_from_csv(file_path, max_instances)[idx-1][1]
-            #print(f"Original Text: {original_text}")
+            print(f"Original Text: {original_text}")
+        """
 
         # Split data into X (features) and y (labels)
         X = [text for _, text in text_list]
         y = [sentiment for sentiment, _ in text_list]
 
-        # Split the data into training, validation, and testing sets
-        X_train, X_temp, y_train, y_temp = train_test_split(X, y, test_size=0.4, random_state=42)
-        X_val, X_test, y_val, y_test = train_test_split(X_temp, y_temp, test_size=0.5, random_state=42)
+        # Split the data into training and testing sets
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
         print("Training set size:", len(X_train))
-        print("Validation set size:", len(X_val))
         print("Testing set size:", len(X_test))
 
-        # Fine-tune BERT model
-        bert_model = fine_tune_bert(X_train, y_train)
+        # Split the training set into a training set and a validation set for hyperparameter tuning
+        X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=0.2, random_state=42)
 
-        # Evaluate BERT model
-        report = evaluate_bert(bert_model, X_test, y_test)
-        print("BERT Model Results (fine-tuning may be needed):")
-        print("Classification Report:")
-        print(report)
+        # Hyperparameter grid for tuning
+        param_grid = {
+            'lr': [1e-5, 5e-5, 1e-4],
+            'batch_size': [16, 32, 64],
+            'num_epochs': [3, 4, 5]
+        }
 
+        # Create a function for model training and evaluation
+        def train_and_evaluate(lr, batch_size, num_epochs):
+            print(f"Hyperparameters: lr={lr}, batch_size={batch_size}, num_epochs={num_epochs}")
+            bert_model = fine_tune_bert(X_train, y_train, lr=lr, batch_size=batch_size, num_epochs=num_epochs)
+            report = evaluate_bert(bert_model, X_val, y_val)
+            return report
+
+        # Create a grid search object
+        grid_search = GridSearchCV(
+            estimator=BertForSequenceClassification,  # Define the BERT estimator class
+            param_grid=param_grid,
+            scoring='f1_macro',
+            cv=3,
+            verbose=2,
+            n_jobs=-1
+        )
+
+        # Perform grid search
+        grid_search.fit(X_train, y_train)
+
+        # Get the best hyperparameters
+        best_lr = grid_search.best_params_['lr']
+        best_batch_size = grid_search.best_params_['batch_size']
+        best_num_epochs = grid_search.best_params_['num_epochs']
+
+        print("Best Hyperparameters:")
+        print(f"Learning Rate: {best_lr}")
+        print(f"Batch Size: {best_batch_size}")
+        print(f"Number of Epochs: {best_num_epochs}")
+
+        # Fine-tune the model with the best hyperparameters on the entire training set
+        best_model = fine_tune_bert(X_train, y_train, lr=best_lr, batch_size=best_batch_size, num_epochs=best_num_epochs)
+
+        # Evaluate the best model on the test set
+        test_report = evaluate_bert(best_model, X_test, y_test)
+
+        print("BERT Model Results with Best Hyperparameters:")
+        print("Classification Report on Test Set:")
+        print(test_report)
     else:
         print("Failed to read text from the CSV file.")
 
 if __name__ == "__main__":
     main()
-
